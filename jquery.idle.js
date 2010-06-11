977d1b52fd5781bb10a31245b98f004ff09c5bd2
Jason McLeod <pixelparty@gmail.com>
2010-06-11 12:20:55 -0400

    Cleaned up some nonos. Updated the API. Big thanks to dcneiner!

--- jquery.idle.js
+++ jquery.idle.js
@@ -1,27 +1,47 @@
-$.fn.idle = function(options) {
-	var isidle = false;
-	var hasMoved = false;
-	var lastMove = (new Date).getTime();
-	var defaults = {
-		after:5000,
-		interval:100
-	}
-	var opts = $.extend(defaults,options);		
-	$(this).bind("mousemove",function() {
-		hasMoved = true;
-		lastMove = (new Date).getTime();
-		if(isidle) {
-			setTimeout(opts.onactive,0);
-			isidle = false;
-		}
-	});
-	setInterval(function() {
-		if((new Date).getTime() - lastMove > opts.after) {
-			if(hasMoved) {
-				setTimeout(opts.onidle,0);
+(function ($) {
+	$.fn.idle = function (onidle, onactive, options) {
+		return this.each(function () {
+			var isidle   = false,
+			    hasMoved = false,
+			    lastMove = (new Date).getTime(),
+			    opts;
+
+			if ($.isPlainObject(onactive)) {
+				options = onactive;
 			}
-			lastMove = (new Date).getTime();
-			isidle = true;
-		}
-	},opts.interval);	
-};
+
+			if (!$.isFunction(onactive)) {
+				onactive = $.noop;
+			}
+
+			opts = $.extend({}, $.fn.idle.defaults, options);
+
+			$(this).bind("mousemove", function () {
+				hasMoved = true;
+				lastMove = (new Date).getTime();
+				if (isidle) {
+					onactive.call(this);
+					isidle = false;
+				}
+			});
+
+			window.setInterval(function() {
+				if ((new Date).getTime() - lastMove > opts.after) {
+					if (hasMoved) {
+						onidle.call(this);
+					}
+					lastMove = (new Date).getTime();
+					isidle = true;
+				}
+			}, opts.interval);
+		});
+	};
+
+	// Set outside so they can be overrided
+	// globally before being called on
+	// an item
+	$.fn.idle.defaults = {
+		after: 5000,
+		interval: 100
+	};
+}(jQuery));
